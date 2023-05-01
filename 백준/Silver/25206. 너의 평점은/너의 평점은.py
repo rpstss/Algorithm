0681cddf79d73615a52a030399c61f25cf368987
rpstss <127698830+rpstss@users.noreply.github.com>
2023-05-01 14:25:39 +0900

    [Silver V] Title: 너의 평점은, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.py
@@ -0,0 +1,14 @@
+sum = 0
+real=0
+dic = {"A+": 4.5, "A0": 4.0, "B+": 3.5, "B0": 3.0, "C+": 2.5,
+       "C0": 2.0, "D+": 1.5, "D0": 1.0, "F": 0.0}
+
+for i in range(20):
+    a, b, c = input().split()
+    if c=="P":
+        continue
+    sum+=float(b)*dic[c]
+    real+=float(b)
+
+print(sum/real)
+
