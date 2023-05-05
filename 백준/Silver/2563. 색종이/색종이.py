39b4c6f59f32aa6dfba0866e94aaa62741a38aaa
rpstss <127698830+rpstss@users.noreply.github.com>
2023-05-05 18:42:27 +0900

    [Silver V] Title: 색종이, Time: 56 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.py
@@ -0,0 +1,12 @@
+n=int(input())
+myList=[[0 for k in range(100)]for k in range(100)]
+for i in range(n):
+    a,b=map(int,input().split())
+    for j in range(b-1,b+9):
+        for h in range(a-1,a+9):
+            myList[j][h]=1
+sum=0
+for i in range(100):
+    for j in range(100):
+        sum += myList[i][j]
+print(sum)
