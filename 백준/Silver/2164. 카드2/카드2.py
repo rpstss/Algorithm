24ede8e7a6fb18e02d74756db128eaba4f060d8a
Park Seo Hyun <127698830+rpstss@users.noreply.github.com>
2023-06-23 17:09:50 +0900

    [Silver IV] Title: 카드2, Time: 216 ms, Memory: 51000 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -0,0 +1,16 @@
+import time
+from collections import deque
+
+
+n=int(input())
+
+
+queue=deque(range(1,n+1))
+
+while len(queue)!=1:
+    queue.popleft()
+    queue.append(queue.popleft())
+    
+
+print(queue.pop())
+
